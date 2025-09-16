6351cc6b517e9fc1afc8a4fc62c608222beb791f
Felipe Saraiva <felipeluizsaraiva71@gmail.com>
2025-09-16 15:39:27 -0300

    Classes Controller criadas com CRUD basico

--- src/main/java/com/api/gerenciamento/servico/controller/ServicoController.java
+++ src/main/java/com/api/gerenciamento/servico/controller/ServicoController.java
@@ -1,4 +1,45 @@
 package com.api.gerenciamento.servico.controller;
 
+
+import com.api.gerenciamento.servico.model.ServicoModel;
+import com.api.gerenciamento.servico.service.ServicoService;
+import org.apache.coyote.Request;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/servico")
 public class ServicoController {
+
+    @Autowired
+    ServicoService servicoService;
+
+
+    @PostMapping
+    public ResponseEntity<?> salvar(@RequestBody ServicoModel servicoModel){
+        ServicoModel salvo = servicoService.salvar(servicoModel);
+        return ResponseEntity.status(201).body(salvo);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ServicoModel>> listarTodos(){
+        return ResponseEntity.ok(servicoService.lerTodos());
+    }
+
+    @PutMapping
+    public ResponseEntity<?> atualizar(@PathVariable Long id, @RequestBody ServicoModel servicoModel){
+        servicoModel.setId(id);
+        ServicoModel atualizado = servicoService.salvar(servicoModel);
+        return ResponseEntity.ok(atualizado);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deletar(@PathVariable Long id){
+        servicoService.deletarPorId(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/api/gerenciamento/usuario/controller/UsuarioController.java
+++ src/main/java/com/api/gerenciamento/usuario/controller/UsuarioController.java
@@ -1,4 +1,43 @@
 package com.api.gerenciamento.usuario.controller;
 
+
+import com.api.gerenciamento.usuario.model.UsuarioModel;
+import com.api.gerenciamento.usuario.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/usuario")
 public class UsuarioController {
+
+    @Autowired
+    UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<?> salvar(@RequestBody UsuarioModel usuarioModel){
+        UsuarioModel salvo = usuarioService.salvar(usuarioModel);
+        return  ResponseEntity.status(201).body(salvo);
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioModel>> listarTodos(){
+        return ResponseEntity.ok(usuarioService.lerTodos());
+    }
+
+    @PutMapping ("{id}")
+    public ResponseEntity<?> atualizarId(@PathVariable Long id, @RequestBody UsuarioModel usuarioModel){
+        usuarioModel.setId(id);
+        UsuarioModel atualizado = usuarioService.salvar(usuarioModel);
+        return ResponseEntity.ok(atualizado);
+    }
+
+    @DeleteMapping ("{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id){
+        usuarioService.deletarPorId(id);
+        return ResponseEntity.noContent().build();
+    }
 }
